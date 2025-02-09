c713f22e2a5bda252eaface0a9454d223a51112e
Grizzly21f <taras.firhuk72@gmail.com>
2025-02-09 18:44:21 +0200

    CLEAN

--- Header.h
+++ Header.h
@@ -1,20 +1,6 @@
 #pragma once
 #include <iostream>
 
-struct Student {
-    std::string name;
-    int age;
-    double grade;
-};
 
 
-Student* createStudents(int size);
-
-void inputStudents(Student* students, int size);
-
-Student findTopStudent(Student* students, int size);
-
-void printTopStudent(const Student& student);
-
-void printStudents(const Student* students, int count);
 
--- main.cpp
+++ main.cpp
@@ -1,25 +1,9 @@
 #include <iostream>
 #include "Header.h"
 
+
 int main() {
-    int size;
 
-    std::cout << "Enter the number of studnets: ";
-    std::cin >> size;
-
-    Student* students = createStudents(size);
-
-    inputStudents(students,size);
-
-    Student topStudent = findTopStudent(students, size);
-
-    printStudents(students, size);
-
-    std::cout << std::endl;
-
-    printTopStudent(topStudent);
-
-    delete[] students;
 
     return 0;
 };
