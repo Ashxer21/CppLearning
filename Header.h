4782826f4bd6c46353775165923fb2d4205425af
Grizzly21f <taras.firhuk72@gmail.com>
2025-02-09 20:12:05 +0200

    2D array & Merge Sort

--- Header.h
+++ Header.h
@@ -1,6 +1,14 @@
 #pragma once
 #include <iostream>
 
+int** create2DArray(int rows,int cols);
 
+void fill2DArray(int** arr, int rows, int cols);
+
+void printArray(int** arr,int rows,int cols);
+
+void mergeSort2D(int** arr, int rows, int cols);
+
+void delete2DArray(int** arr,int rows);
 
 
--- main.cpp
+++ main.cpp
@@ -3,7 +3,22 @@
 
 
 int main() {
+    int rows,cols;
+    std::cout << "Enter a rows for 2D-array: ";
+    std::cin >> rows;
 
+    std::cout << "Enter a cols for 2D-array: ";
+    std::cin >> cols;
+
+    int** arr = create2DArray(rows,cols);
+
+    fill2DArray(arr,rows,cols);
+
+    mergeSort2D(arr,rows,cols);
+
+    printArray(arr,rows,cols);
+
+    delete2DArray(arr,rows);
 
     return 0;
 };
