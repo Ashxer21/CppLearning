92783681e606dac15e4063d29c3a2c7f78b6c727
Ashxer <taras,firhuk72@gmail.com>
2025-02-16 21:30:34 +0200

    Students class

--- Header.h
+++ Header.h
@@ -1,6 +1,18 @@
 #pragma once
 #include <iostream>
 
+class Student {
+private:
+    char name[20] = "NoName";
+    int age = 0;
 
+public:
+    void Show();
+    void ChangeName(const char* newName);
+    void ChangeAge(int newAge);
+    void EnterName();
+    void EnterAge();
+    int GetAge() { return age; };
+};
 
 
--- main.cpp
+++ main.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
 #include "Header.h"
 
+void Initialize(Student& student) {
+    student.EnterAge();
+    student.EnterName();
+}
+
+
+void Initialize(Student students[], size_t size) {
+    for (size_t i = 0; i < size; i++) {
+        std::cout << "Student #" << i + 1 << ":\n";
+        Initialize(students[i]);
+    }
+}
+
+
+void Show(Student students[], size_t size) {
+    for (size_t i = 0; i < size; i++) {
+        std::cout << "Student #" << i + 1 << ":\n";
+        students[i].Show();
+    }
+}
+
 
 int main() {
+    std::cout << "Enter amount of students: ";
+    size_t size;
+    std::cin >> size;
 
+    Student* students = new Student[size];
+
+    Initialize(students,size);
+    Show(students,size);
+
+
+    delete[] students;
 
     return 0;
 };
